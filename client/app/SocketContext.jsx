79d691e4d61afbcccec81cf2df15a29ee3642698
trane7776 <izbasarov.t@edu.rea.ru>
2024-02-29 13:34:38 +0300

    updated

--- client/app/SocketContext.jsx
+++ client/app/SocketContext.jsx
@@ -46,7 +46,33 @@ const ContextProvider = ({ children }) => {
       trickle: true,
       stream,
       config: {
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+        iceServers: [
+          {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+          },
+          {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+          },
+          {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+          },
+          {
+            url: 'turn:turn.bistri.com:80',
+            credential: 'homeo',
+            username: 'homeo',
+          },
+          {
+            url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            credential: 'webrtc',
+            username: 'webrtc',
+          },
+        ],
       },
     });
     console.log('sss');
@@ -69,7 +95,33 @@ const ContextProvider = ({ children }) => {
       trickle: true,
       stream,
       config: {
-        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+        iceServers: [
+          {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+          },
+          {
+            url: 'turn:192.158.29.39:3478?transport=udp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+          },
+          {
+            url: 'turn:192.158.29.39:3478?transport=tcp',
+            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            username: '28224511:1379330808',
+          },
+          {
+            url: 'turn:turn.bistri.com:80',
+            credential: 'homeo',
+            username: 'homeo',
+          },
+          {
+            url: 'turn:turn.anyfirewall.com:443?transport=tcp',
+            credential: 'webrtc',
+            username: 'webrtc',
+          },
+        ],
       },
     });
 
