1ec0a312fa2c42d7ed1c9935c4b2ee0cc93dd2f8
trane7776 <izbasarov.t@edu.rea.ru>
2024-02-29 12:54:48 +0300

    updated

--- client/app/SocketContext.jsx
+++ client/app/SocketContext.jsx
@@ -48,7 +48,10 @@ const ContextProvider = ({ children }) => {
       config: {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
-          { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+          { urls: 'stun:stun1.l.google.com:19302' },
+          { urls: 'stun:stun2.l.google.com:19302' },
+          { urls: 'stun:stun3.l.google.com:19302' },
+          { urls: 'stun:stun4.l.google.com:19302' },
         ],
       },
     });
@@ -74,7 +77,10 @@ const ContextProvider = ({ children }) => {
       config: {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
-          { urls: 'stun:global.stun.twilio.com:3478?transport=udp' },
+          { urls: 'stun:stun1.l.google.com:19302' },
+          { urls: 'stun:stun2.l.google.com:19302' },
+          { urls: 'stun:stun3.l.google.com:19302' },
+          { urls: 'stun:stun4.l.google.com:19302' },
         ],
       },
     });
